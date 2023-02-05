86942922a4049b51095ccb21445c5c40df7d849b
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-05 23:01:04 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Values from "values.js";
 import SingleColor from "./SingleColor";
 
 export default function App() {
+  const [list, setList] = useState(new Values("#f15025").all(10));
+  const [color, setColor] = useState();
+  const [error, setError] = useState(false);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("testing");
+    try {
+      let colorgen = new Values(color);
+      setList(colorgen.all(10));
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   }
   return (
     <>
       <section className="header">
         <h2>Color Generator</h2>
         <form onSubmit={handleSubmit}>
-          <input placeholder="#f15025" />
+          <input
+            type="text"
+            placeholder="#f15025"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={error ? "error" : null}
+          />
           <button type="submit">Submit</button>
         </form>
       </section>
       <section className="colors-con">
-        
+        {list.map((col, index) => {
+          return <SingleColor key={index} col={col} index={index} />;
+        })}
       </section>
     </>
   );
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-export default function SingleColor(){
-    return (
-        <article className='single-color'>
-            <p className='weight'></p>
-            <p className='color'></p>
-            <p className='copy'></p>
-        </article>
-    )
+export default function SingleColor(props) {
+  const [copy, setCopy] = useState(false);
+  const { rgb, weight, hex } = props.col;
+  const bcg = rgb.join(",");
+  const hexValue = `#${hex}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopy(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [copy]);
+  return (
+    <article
+      className={`single-color ${props.index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setCopy(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="weight">{weight}%</p>
+      <p className="color">{hexValue}</p>
+      <p className="copy">{copy && "copy to clipboard"}</p>
+    </article>
+  );
 }
